508eb93b9ed1aa833b0bca0e2ac05d6bdc39492f
Paul Williams <thecodinganthropoid@gmail.com>
2025-04-25 04:14:09 -0400

    variables and scope practice task

--- SampleFolder/Scope/scope_lab.js
+++ SampleFolder/Scope/scope_lab.js
@@ -17,8 +17,8 @@ console.log(globalLet); // Output: "I'm also global, but scoped with let"
 console.log(globalConst); // Output: "I'm a global constant"
 
 //Block Scope
-// console.log(blockVar);
-// console.log(blockLet);
+console.log(blockVar); // Exists in this scope
+// console.log(blockLet); // Throws ReferenceError
 
 function show(){
     var functionVar = "I'm a block-scoped var";
@@ -27,6 +27,38 @@ function show(){
     }
     show();
     
-console.log(functionVar); // Throws ReferenceError
-console.log(functionLet); // Throws ReferenceError
-console.log(functionConst); // Throws ReferenceError
+// console.log(functionVar); // Throws ReferenceError
+// console.log(functionLet); // Throws ReferenceError
+// console.log(functionConst); // Throws ReferenceError
+
+/*
+== Practice Task ==
+1)   Create one block using {} and declare variables using let, const, and var
+2)   Then, try to reassign these variables within the curly braces {}. Check 
+     for any errors that may occur.
+3)   Next, try to reassign the same variables outside the block {} and check 
+     if assignment outside the sope of variables where the variables are 
+     assigned can be done or not.
+*/
+
+// 1
+{
+    let letVar = "I'm a let variable in a block";
+    const constVar = "I'm a const variable in a block";
+    var varVar = "I'm a var variable in a block";
+    // 2
+    letVar = "I have been re-assigned";
+    // constVar = "This should not work"; // Uncaught TypeError: invalid assignment to const 'constVar'
+    varVar = "I have been reassigned";
+}
+// 3
+letVar = "Variable doesn't exist";
+constVar = "Another 'Uncaught Type Error'";
+varVar = "This one should be fine";
+console.log(letVar);
+console.log(constVar);
+console.log(varVar);
+// It appears that, rather than re-assigning letVar and constVar from the block
+// - as they do not exist outside the block - they have been given new 
+// variables that defualt to the var type. Based on previous behaviour @line_20
+// varVar is actually re-assigned.
